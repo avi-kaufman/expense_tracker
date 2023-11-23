7fc3619921df507effac12242398e74d3385cda0
Avi Kaufman <avrham.kaufman@post.idc.ac.il>
2023-11-23 14:53:23 +0200

    implement spent in analistics file, need to implement the logic on start end date

--- backend/analistics.py
+++ backend/analistics.py
@@ -1,7 +1,19 @@
-def sum():
-    pass
+import csv
 
-def mean():
+EXPENSES_FILE = '../dataBase/expense.csv'
+
+def spent_on_category(user_id, start_date, end_date, category_name):
+    spent = 0
+    with open(EXPENSES_FILE, 'r') as csvfile:
+        reader = csv.reader(csvfile)     
+        next(reader) # reed the filds
+        for row in reader:
+            if row[1] == user_id and row[5] >= start_date and row[5]<=end_date and row[3] == category_name:# need to implement the age condition
+                spent += float(row[2])
+
+    return spent            
+
+def mean(user_id, age, category_id):
     pass
 
 def main():
